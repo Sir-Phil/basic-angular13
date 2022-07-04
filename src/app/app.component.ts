be4ff53c6c57dffae67c493ce68e82b6f21b8606
Sir-Phil <52827424+Sir-Phil@users.noreply.github.com>
2022-07-04 15:11:53 +0100

    Understanding Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'cp-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'company-form';
+  pageTitle = 'company-form';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import {CardModule} from 'primeng/card';
+import {InputTextModule} from 'primeng/inputtext';
+import {TableModule} from 'primeng/table';
+import {ButtonModule} from 'primeng/button';
+import { ProductsComponent } from './components/products/products.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CartsComponent } from './components/carts/carts.component';
+import { FormsModule } from '@angular/forms';
+
+
+
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    HeaderComponent,
+    CartsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardModule,
+    InputTextModule,
+    TableModule,
+    ButtonModule,
+    HttpClientModule,
+    FormsModule 
+
   ],
   providers: [],
   bootstrap: [AppComponent]
